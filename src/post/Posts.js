6f196efb48fc5eb621cb8ca220e1ad91ac063837
Vladimir RM <shvdrummer@gmail.com>
2023-02-12 15:05:17 +0200

    add useSelector

--- src/post/Posts.js
+++ src/post/Posts.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import PostItem from './PostItem'
-import { useDispatch } from 'react-redux'
-import { getPosts } from './postSlice'
-const Posts = ({Posts}) => {
-
-    const dispatch = useDispatch()
+import React from "react";
+import PostItem from "./PostItem";
+import { useDispatch } from "react-redux";
+import { getPosts, useSelector } from "./postSlice";
+const Posts = ({ Posts }) => {
+  const dispatch = useDispatch();
+  const post = useSelector((state) => state.post.posts);
   return (
     <div>
-        <button onClick={()=>dispatch(getPosts())}>
-
-        </button>
-        <PostItem/>
+      <button onClick={() => dispatch(getPosts())}></button>
+      <PostItem />
     </div>
-  )
-}
+  );
+};
 
-export default Posts
+export default Posts;
